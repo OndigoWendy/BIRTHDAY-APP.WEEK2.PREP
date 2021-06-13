47ae328ff42a7fc92df006a22471704512f6bade
Ondigo Wendy <wendy.ondigo@student.moringaschool.com>
2021-06-13 11:06:21 +0300

    Obtain the user inputs from the html page and declare them as variables in the javascript file using dom traversal.

---
+++ js/script.js
@@ -0,0 +1,5 @@
+month = document.form.month.selectedIndex;
+month = document.form.month.options[month].value;
+day = document.form.day.selectedIndex;
+day = document.form.day.options[day].value;
+year = document.form.year.value;
